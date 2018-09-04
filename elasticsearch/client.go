4455877dd0b95c93e2a679cbdc0b1aa8efd28c67
Jared Carey <jared.carey@elastic.co>
2018-09-03 23:18:26 -0600

    remove endpoint

--- elasticsearch/client.go
+++ elasticsearch/client.go
@@ -10,8 +10,8 @@ var (
 	// Client is a connection handle
 	// for elasticsearch
 	Client, err = elastic.NewClient(
-		elastic.SetURL("https://5084f6be56d6407b80dcc1fa9c1628f9.us-central1.gcp.cloud.es.io:9243"),
-		elastic.SetBasicAuth("elastic", "NiFgH8cFEdpGeoHb9FDAAjV1"),
+		elastic.SetURL("http://localhost:9200"),
+		elastic.SetBasicAuth("elastic", "changeme"),
 		elastic.SetSniff(false),
 		elastic.SetHealthcheckInterval(10*time.Second),
 		elastic.SetGzip(true),
